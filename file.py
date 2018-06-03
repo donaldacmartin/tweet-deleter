ee1f19a1be5c8a1cd4f9be8daa937caf5f184a27
Donald AC Martin <donald@martin.scot>
2018-06-03 09:14:59 +0100

    Making more functional

--- file.py
+++ file.py
@@ -26,28 +26,23 @@ def read_file(filename):
         
     return json
     
-def parse_twitter_data(json):
-    tweets = []
+def parse_tweet(tweet_json):
+    date = tweet_json["created_at"]
+    id = str(tweet_json["id"])
+    return {"date": parse(date).replace(tzinfo=None), "id": id} 
     
-    if json:
-        for tweet in json:
-            date = tweet["created_at"]
-            id = str(tweet["id"])
-            tweets.append({"date": parse(date).replace(tzinfo=None), "id": id})
-        
-    return tweets
+def parse_twitter_data(json):
+    return [parse_tweet(tweet_json) for tweet_json in json]
     
 def filter_new_tweets(tweets, number_months):
     today = datetime.now()
     time_delta = timedelta(days = (number_months * 30))
+    print(tweets)
     return [tweet for tweet in tweets if today - tweet["date"] > time_delta]
     
 def get_tweets(export_path, number_months):
     files = get_all_files(export_path)
-    tweets = []
-    
-    for file in files:
-        json = read_file(file)
-        tweets += parse_twitter_data(json)
-    
+    jsons = [read_file(file) for file in files]
+    tweets_list = [parse_twitter_data(json) for json in jsons]
+    tweets = [tweet for tweets in tweets_list for tweet in tweets]
     return filter_new_tweets(tweets, number_months)
